d8e62c4f88ac4549592daf105f3460a9786686a0
Christian <matschke.berlin@icloud.com>
2022-11-28 14:27:09 +0100

    Update Queries.kt

--- src/main/kotlin/so/arctic/elasticsearch/queries/Queries.kt
+++ src/main/kotlin/so/arctic/elasticsearch/queries/Queries.kt
@@ -1,12 +1,11 @@
 package so.arctic.elasticsearch.queries
 
 import co.elastic.clients.elasticsearch._types.FieldValue
-import co.elastic.clients.elasticsearch._types.query_dsl.Query
-import co.elastic.clients.elasticsearch._types.query_dsl.QueryBuilders
 import co.elastic.clients.elasticsearch._types.query_dsl.TermsQuery
 
 
-fun TermsQuery.Builder.termsQuery(field: String, values: List<FieldValue>): Query =
-   QueryBuilders.terms { t ->
-      t.field(field).terms { ts -> ts.value(values) }
+fun termsQuery(field: String, values: List<FieldValue>): TermsQuery =
+   TermsQuery.of { t ->
+      t.field(field)
+         .terms { ts -> ts.value(values) }
    }
