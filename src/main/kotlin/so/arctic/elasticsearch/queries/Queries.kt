b264f67d6a9e23367b9729376b7000efe40f4014
Christian <matschke.berlin@icloud.com>
2022-12-01 15:24:31 +0100

    feat: new DSL nested Query builder

--- src/main/kotlin/so/arctic/elasticsearch/queries/Queries.kt
+++ src/main/kotlin/so/arctic/elasticsearch/queries/Queries.kt
@@ -1,7 +1,10 @@
 package so.arctic.elasticsearch.queries
 
 import co.elastic.clients.elasticsearch._types.FieldValue
+import co.elastic.clients.elasticsearch._types.query_dsl.NestedQuery
+import co.elastic.clients.elasticsearch._types.query_dsl.Query
 import co.elastic.clients.elasticsearch._types.query_dsl.TermsQuery
+import co.elastic.clients.util.ObjectBuilder
 
 
 fun termsQuery(field: String, values: List<FieldValue>): TermsQuery =
@@ -9,3 +12,19 @@ fun termsQuery(field: String, values: List<FieldValue>): TermsQuery =
       t.field(field)
          .terms { ts -> ts.value(values) }
    }
+
+@DslMarker
+annotation class QueryDSL
+
+@QueryDSL
+class QueryBuilder {
+   fun nested(apply: NestedQuery.Builder.() -> Unit): ObjectBuilder<Query> = Query.Builder().nested {
+      it.apply(apply)
+   }
+
+   companion object {
+      inline fun builder(block: QueryBuilder.() -> Unit) = QueryBuilder().apply(block)
+   }
+}
+
+
